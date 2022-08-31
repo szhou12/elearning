fc4b4a567b1b5d7a35b7f84fca26de6b1bce29df
Jiaying Shen <78494129+pattypoem@users.noreply.github.com>
2022-08-31 15:13:45 -0500

    Update urls.py

--- immersioned/urls.py
+++ immersioned/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('itutorials/<int:pk>/', views.ITutorialDetail.as_view(), name = "itutorial-detail"),
     path('sample_data/', views.sample_data, name='sample_data'),
     path('sample_data2/', views.sample_data2, name='sample_data2'),
+    path('listnotes/', views.LNotesList.as_view(), name='lnotes'),
     # Student URLs
     path('learner/', views.home_learner, name='learner'),
     path('ltutorial/',views.ltutorial, name='ltutorial'),
